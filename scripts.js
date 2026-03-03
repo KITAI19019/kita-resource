59de9f00013bb173e42385dc511f0878060c1b06
KITAI19019 <147451303+KITAI19019@users.noreply.github.com>
2026-03-03 21:26:28 +0800

    访客记录

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
-// 示例：下载时弹窗
-function downloadAlert() {
+function downloadAlert(event) {
+    gtag('event', 'download', {  // 发事件到Google
+        'file_name': event.target.href.split('/').pop(),  // 取文件名
+        'category': '资源下载'
+    });
     alert("感谢下载！欢迎反馈。");
 }
+// 加到链接
+document.querySelectorAll('a[download]').forEach(link => {
+    link.addEventListener('click', downloadAlert);
+});
 
 // 选所有下载链接，加点击事件
 const downloadLinks = document.querySelectorAll('a.transl[download]');
@@ -36,4 +43,3 @@ function updateVisitorCount() {
     document.getElementById('visitorCount').textContent = count;
 }
 
-window.addEventListener('load', updateVisitorCount);
